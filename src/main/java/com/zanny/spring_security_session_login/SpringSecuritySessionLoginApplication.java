66700c2263e892ea68f38f1304968990aa03406e
zanny31 <jianb0331@naver.com>
2025-04-06 15:51:05 +0900

    chore: Spring Boot 프로젝트 초기 설정

---
+++ src/main/java/com/zanny/spring_security_session_login/SpringSecuritySessionLoginApplication.java
@@ -0,0 +1,13 @@
+package com.zanny.spring_security_session_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySessionLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuritySessionLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zanny/spring_security_session_login/SpringSecuritySessionLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zanny.spring_security_session_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySessionLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
