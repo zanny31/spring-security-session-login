cce7acb2783dcb342b17297c082141d895f5e48c
zanny31 <jianb0331@naver.com>
2025-04-06 17:30:40 +0900

    feat: LoginUser 엔티티 정의

---
+++ src/main/java/com/zanny/login/domain/LoginUser.java
@@ -0,0 +1,34 @@
+package com.zanny.login.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LoginUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String role;
+
+    @Builder
+    public LoginUser(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+}
